3cc8c803bd1743ce88a9d8631976804af8968e94
M1rotvorez <volkik91@gmail.com>
2017-03-09 22:38:45 +0600

    MD-88  Fixed mobile width

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,11 @@
 /**
  * Created by M1rotvorez on 3/7/17.
  */
+
+new Vivus('my-svg', {
+		duration: 200,
+		onReady: function (vivusInstance) {
+				vivusInstance.el.style.visibility = 'visible';
+		}
+});
+// new Vivus('t2', {type: 'delayed', duration: 100, start: 'autostart'});
